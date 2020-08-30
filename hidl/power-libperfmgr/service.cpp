LineageOS/android_hardware_xiaomi

power-libperfmgr: adapt and rebrand for hardware/xiaomi
Change-Id: I8aa9a712ac7cbc6085928f7cdb9e6ada8ed5e3fe

hidl/power-libperfmgr/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.3-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.xiaomi-libperfmgr"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
@@ -34,7 +34,7 @@ using android::hardware::power::V1_3::IPower;
 using android::hardware::power::V1_3::implementation::Power;
 
 int main(int /* argc */, char ** /* argv */) {
-    ALOGI("Power HAL Service 1.3 for Pixel is starting.");
+    ALOGI("Power HAL Service 1.3 for Samsung is starting.");
 
     android::sp<IPower> service = new Power();
     if (service == nullptr) {

sm8150-common/hardware_xiaomi

Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

hidl/power-libperfmgr/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.3-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.xiaomi-libperfmgr"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
@@ -34,7 +34,7 @@ using android::hardware::power::V1_3::IPower;
 using android::hardware::power::V1_3::implementation::Power;
 
 int main(int /* argc */, char ** /* argv */) {
-    ALOGI("Power HAL Service 1.3 for Pixel is starting.");
+    ALOGI("Power HAL Service 1.3 for Xiaomi is starting.");
 
     android::sp<IPower> service = new Power();
     if (service == nullptr) {